a06ea200c26abe178cb9ff75c306f25ee10b865a
jk21-HH <jeanya12@gmail.com>
2023-09-11 10:41:55 +0300

    add comments

--- server/app/Http/Controllers/Api/TrainerController.php
+++ server/app/Http/Controllers/Api/TrainerController.php
@@ -55,18 +55,23 @@ class TrainerController extends Controller
     public function updateTrainer(Request $request, int $trainerId): JsonResponse
     {
         // Get the authenticated trainer
+        
         $authenticatedTrainer = Auth::user();
-    
+
         // Check if the authenticated trainer matches the trainer being updated
+        
         if ($authenticatedTrainer->id !== $trainerId) {
+            
             // Return 403 Forbidden
+            
             return response()->json([
                 'status' => 'fail',
                 'message' => 'You are not authorized to update this trainer data'
             ], 403);
         }
-    
+
         // Define validation rules
+        
         $rules = [
             'email' => [
                 'required',
@@ -75,20 +80,24 @@ class TrainerController extends Controller
             ],
             'full_name' => 'required|string|max:255',
         ];
-    
+
         // Check if at least one of the fields (email, full_name, password) is provided in the request
+        
         if ($request->filled('email') || $request->filled('full_name') || $request->filled('password')) {
-            // Email field should be unique if updated
+            // email field should be unique if updated
+            
             $rules['email'][] = function ($attribute, $value, $fail) use ($authenticatedTrainer) {
                 if ($value === $authenticatedTrainer->email) {
                     $fail('Email must be different from your current email');
                 }
             };
-        
-            // Full Name field should be required if updated
+
+            // full name field should be required if updated
+            
             $rules['full_name'] = 'string|max:255';
-        
-            // Password field should be required if updated and different from the current password
+
+            // password field should be required if updated and different from the current password
+            
             $rules['password'] = [
                 'string',
                 'min:8',
@@ -99,36 +108,45 @@ class TrainerController extends Controller
                 },
             ];
         } else {
-            // If none of the fields are provided, return a validation error
+            
+            // Return 422 Server can't process your request
+            
             return response()->json([
                 'status' => 'fail',
                 'message' => 'At least one of the fields is required'
             ], 422);
         }
-    
-        // Validate the request data
+
+        // validate the request data
+        
         $validatedData = $request->validate($rules);
-    
-        // Hash the new password with bcrypt if provided
+
+        // hash the new password with bcrypt if provided
+        
         if ($request->filled('password')) {
             $validatedData['password'] = \Hash::make($validatedData['password']);
         }
-    
-        // Update the fields of the trainer
+
+        // update the fields of the trainer
+        
         $trainer = Trainer::find($trainerId);
-    
+
         if (!$trainer) {
+            
             // Return 404 Not Found status code
+            
             return response()->json([
                 'status' => 'fail',
                 'message' => 'Trainer not found'
             ], 404);
         }
-    
+
         // Update the trainer's data
+       
         $trainer->update($validatedData);
-    
+
         // OK status code
+       
         return response()->json([
             'status' => 'success',
             'message' => 'Trainer data updated successfully',
